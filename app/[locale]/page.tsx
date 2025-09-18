39c40cfd962616ca8b582e3cd7d3e2c130d9c8bf
sidi-cnm <sdeiebeidy@gmail.com>
2025-09-18 18:57:58 +0000

    fix:ispublished changer to true

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -38,7 +38,7 @@ export default async function Home({
   const skip = (currentPage - 1) * itemsPerPage;
 
   // ----- Query Mongo -----
-  const query: Record<string, any> = { status: "active", isPublished: false };
+  const query: Record<string, any> = { status: "active", isPublished: true };
 
   if (sp.typeAnnonceId)   query.typeAnnonceId   = sp.typeAnnonceId;
   if (sp.categorieId)     query.categorieId     = sp.categorieId;
