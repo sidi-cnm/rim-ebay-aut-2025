faeb092dcc0cfcf33463f559412f6f2d22efc4df
ely cheikh <eouldsmail@gmail.com>
2025-12-14 02:26:00 +0000

    logs api errors

--- app/[locale]/p/api/tursor/lieux/tursoClient.ts
+++ app/[locale]/p/api/tursor/lieux/tursoClient.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@libsql/client";
+console.log("TURSO_DATABASE_URL_LIEUX:", process.env.TURSO_DATABASE_URL_LIEUX);
+console.log("TURSO_AUTH_TOKEN_LIEUX:", process.env.TURSO_AUTH_TOKEN_LIEUX);
 export const turso = createClient({
   url: process.env.TURSO_DATABASE_URL_LIEUX!,
   authToken: process.env.TURSO_AUTH_TOKEN_LIEUX!,
